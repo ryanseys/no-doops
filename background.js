5e11291239fbfcb81673835163ef1917223c0e84
Ryan Seys <ryanseys@google.com>
2016-11-01 00:56:44 -0700

    no semicolon

--- background.js
+++ background.js
@@ -22,7 +22,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query(query, function (tabs) {
     var urls = {}
     tabs.forEach(function (tab, index) {
-      var tabUrl = tab.url;
+      var tabUrl = tab.url
       if (urls[tabUrl] === true) {
         // tab already exists, so remove it.
         chrome.tabs.remove(tab.id)
