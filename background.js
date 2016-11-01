07c9be5a6a7871931fef47fde7f310815344af21
Ryan Seys <ryanseys@google.com>
2016-11-01 00:55:57 -0700

    Do not try to be smart

--- background.js
+++ background.js
@@ -22,7 +22,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query(query, function (tabs) {
     var urls = {}
     tabs.forEach(function (tab, index) {
-      var tabUrl = tab.url.split('#')[0]
+      var tabUrl = tab.url;
       if (urls[tabUrl] === true) {
         // tab already exists, so remove it.
         chrome.tabs.remove(tab.id)
